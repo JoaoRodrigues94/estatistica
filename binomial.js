8955c534cb746c7be4c03c624a9f32340ac1d132
João Paulo Fernandes Rodrigues <61790808+JoaoRodrigues94@users.noreply.github.com>
2020-11-02 00:40:37 -0300

    01/11

--- binomial.js
+++ binomial.js
@@ -35,9 +35,60 @@ function Binomial(){
         div = div / analise
         let res = 0
         res = div * (s ** y) * (f ** subtracao)
+        console.log("Lista: " + div +" X " +s + "^" + y + " X " + f + "^" + subtracao)
+        console.log(`res: ${s**y} X ${f ** subtracao}`)
+        console.log(res)
         resultado += res
     }
     resultado *= 100
-    console.log(resultado.toFixed(2) + " %")
     document.getElementById('Resultado').innerHTML = "Probabilidade: " + resultado.toFixed(2) + " %"
+}
+
+function Mostra(){
+    let escolha = document.getElementById('Escolha').value
+    
+    if(escolha == "Entre"){
+        document.getElementById("Ent").style = "visibility: visible;"
+    } else{
+        document.getElementById("Ent").style = "visibility: hidden;"
+    }
+}
+
+function Uniforme(){
+    const menor = document.getElementById('Menor').value
+    const maior = document.getElementById('Maior').value
+    const inter = document.getElementById('Valor').value
+    const verifica = document.getElementById('Escolha').value 
+    const valor = document.getElementById('Valor').value
+    const ent = document.getElementById('Ent').value
+
+    let intervalo = 0
+    let dividendo = 0
+    let media = (parseFloat(maior) + parseFloat(menor)) / 2 
+    let variancia = Math.sqrt(((parseFloat(maior) - parseFloat(menor)) ** 2) / 12)
+    let DevPadrao = (variancia / media) * 100
+    console.log(ent)
+    if(verifica == 'Maior'){
+        intervalo = parseFloat(maior) - parseFloat(inter)
+        dividendo = parseFloat(maior) - parseFloat(menor)
+    }
+    else if(verifica == 'Entre'){
+        intervalo = parseFloat(ent) - parseFloat(valor) 
+        dividendo = parseFloat(maior) - parseFloat(menor)
+    }
+    else if(verifica == 'Menor'){
+        intervalo = parseFloat(menor) - parseFloat(inter)
+        dividendo = parseFloat(menor) - parseFloat(maior)
+    }
+
+    let prob = 1 / dividendo
+    prob *= intervalo
+    prob *= 100
+
+    console.log('Média: ' + media)
+    console.log('Variancia: ' + variancia.toFixed(2))
+    console.log('Desvio Padrão: ' + DevPadrao.toFixed(2) + "%")
+    console.log("Probalidade" + prob + " %")
+
+    document.getElementById('Resul').innerHTML = "Média: " + media + "</br> Variância: " + variancia.toFixed(2) + "</br>Desvio Padrão: " + DevPadrao.toFixed(2) + "%" + "</br> Probabilidade: " + prob + "%"
 }
